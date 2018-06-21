608526fd532e7526f35d6e7ce7c4bb40925e48d0
Alberto Saravia <alberto.saravia@ttu.edu>
2018-06-21 15:24:20 -0500

    added valid and invalid inputs

--- Quiz3.py
+++ Quiz3.py
@@ -17,7 +17,12 @@ def dot(vector01,vector02):
 
 
 
-A = [1,1,1]
-B = [1,1,1]
+validInput1 = [1,1,1]
+validInput2 = [1,1,1]
+invalidInput1 =  [1,2,3]
+invalidInput2 = [1,2]
 
-print(dot(A,B))
+print(dot(validInput1,validInput2))
+print(dot(invalalidInput1,invalidInput2))
+
+
