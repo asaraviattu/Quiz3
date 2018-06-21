8fa243af60a24ef38b28799fb3ae1d11e9f162dd
Alberto Saravia <alberto.saravia@ttu.edu>
2018-06-21 16:26:57 -0500

    Problem number 3 complete with valid and invalid inputs

--- Quiz3.py
+++ Quiz3.py
@@ -16,6 +16,7 @@ def dot(vector01,vector02):
 		return sum
 
 
+#valid and invalid inputs to test and their respective tests commented out below.
 
 validInput1 = [1,1,1]
 validInput2 = [1,1,1]
@@ -31,7 +32,9 @@ invalidInput2 = [1,2]
 
 def vecSubtract(vector03,vector04):
 	'''
-	This functions takes two vectors as arguments and returns a new vector with the respective subraction. 
+	This functions takes two vectors as arguments and returns a new vector with the respective subraction. We
+	first create an empty list, then check for dimensions of inputs and if they checkout, execute subraction 
+	and append element's subraction onto empty list in order to create new list with subraction finished.
 
 	'''
 	newVec = []
@@ -46,6 +49,7 @@ def vecSubtract(vector03,vector04):
 
 
 
+#valid and invalid inputs to test and their respective tests commented out below.
 
 validInput3 = [3,4,5]
 validInput4 = [1,1,1]
@@ -53,4 +57,60 @@ invalidInput3 = [2,2,2]
 invalidInput4 = [1,1]
 
 #print(vecSubtract(validInput3,validInput4))
-print(vecSubtract(invalidInput3,invalidInput4))
+#print(vecSubtract(invalidInput3,invalidInput4))
+
+
+
+def scalarVecMulti(scalar01,vector05):
+	'''
+	This functions takes a scalar and a vector as arguments and returns a new vector with the respective
+	multiplication executed. The if-else statement checks for the scalar and vector to bbe either integers
+	or lists, if they are then we proceed to execute the multiplication by appending to an empty list the
+	result of the executed multiplication between the scalar and each individual element in the vector.
+
+	'''
+	newVec = []
+	if type(scalar01) != int or type(vector05) != list:
+		print("invalid input")
+	else:
+		for i in range(len(vector05)):
+			mult = vector05[i]*scalar01
+			newVec.append(mult)
+
+		return newVec
+
+
+validInput5 = 2
+validInput6 = [1,2,3]
+invalidInput5 = [1,1,1]
+invalidInput6 = 3
+
+#valid and invalid inputs to test and their respective tests commented out below.
+
+#print(scalarVecMulti(validInput5,validInput6))
+#print(scalarVecMulti(invalidInput5,invalidInput6))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
